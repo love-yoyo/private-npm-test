4ee5cc7975753e28c4ea46923741c785433e125e
King <yao_jin90@163.com>
2019-11-23 11:38:45 +0800

    feat: change version to 1.0.1 in package.json

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = { name: "module-a" }
+module.exports = { 
+    name: "module-a",
+    change: function() {
+        // change status
+        console.log("have changed the version to 1.0.1 in package.json");
+        return "version => 1.0.1";
+    }
+}
